806173d77bb970e0fc255b5a30b5419580906c17
GabriellyVitoria5 <assuncaogabrielly5@gmail.com>
2025-02-17 18:59:17 -0300

    Atualizando banco para salvar as partidas do jogfador

--- app/src/main/java/com/ifmg/termix/localdata/DatabaseContract.kt
+++ app/src/main/java/com/ifmg/termix/localdata/DatabaseContract.kt
@@ -27,19 +27,47 @@ class DatabaseContract {
 
     // Definir a estrutura da tabela para armazenar as palavras escritas pelo jogador em uma jogada
     object PLAYER_WORDS {
-        const val TABLE_NAME = "playerwords"
+        const val TABLE_NAME = "player_words"
 
         const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_GAME_ID = "game_id" // Identificar uma partida
         const val COLUMN_NAME_WORD = "word"
         const val COLUMN_NAME_ATTEMPT = "attempt"
+        const val COLUMN_NAME_GAME_MODE = "game_mode"
 
         const val SQL_CREATE = "CREATE TABLE IF NOT EXISTS $TABLE_NAME (" +
                 "$COLUMN_NAME_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$COLUMN_NAME_GAME_ID INTEGER NOT NULL, " +
                 "$COLUMN_NAME_WORD TEXT NOT NULL, " +
-                "$COLUMN_NAME_ATTEMPT INTEGER NOT NULL);"
+                "$COLUMN_NAME_ATTEMPT INTEGER NOT NULL, " +
+                "$COLUMN_NAME_GAME_MODE TEXT NOT NULL, " +
+                "FOREIGN KEY($COLUMN_NAME_GAME_ID) REFERENCES ${GAME_SESSION.TABLE_NAME}(${GAME_SESSION.COLUMN_NAME_ID}) ON DELETE CASCADE);"
 
         const val SQL_DROP = "DROP TABLE IF EXISTS $TABLE_NAME;"
     }
+
+    // Definir a estrutura da tabela que guarda os dados de uma partida
+    object GAME_SESSION {
+        const val TABLE_NAME = "game_session"
+
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_GAME_DATE = "game_date"
+        const val COLUMN_NAME_MODE = "mode"
+        const val COLUMN_NAME_WORD = "word" // Palavra sorteada para o jogo
+        const val COLUMN_NAME_ATTEMPT = "attempt"
+        const val COLUMN_NAME_STATUS = "status"
+
+        const val SQL_CREATE = "CREATE TABLE IF NOT EXISTS $TABLE_NAME (" +
+                "$COLUMN_NAME_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$COLUMN_NAME_GAME_DATE TEXT NOT NULL, " +
+                "$COLUMN_NAME_MODE TEXT NOT NULL, " +
+                "$COLUMN_NAME_WORD TEXT NOT NULL, " +
+                "$COLUMN_NAME_ATTEMPT INTEGER NOT NULL, " +
+                "$COLUMN_NAME_STATUS TEXT NOT NULL DEFAULT 'nao_iniciada');"
+
+        const val SQL_DROP = "DROP TABLE IF EXISTS $TABLE_NAME;"
+    }
+
 
     // Definir a estrutura da tabela de perfis de jogadores
     object PROFILE {
